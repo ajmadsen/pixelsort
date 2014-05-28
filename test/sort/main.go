719620fc61c04dd2019db4ade7658d8523ad8324
Adam Madsen <madsen.adam@gmail.com>
2014-05-28 04:20:06 -0700

    Add additional by-pixel method

--- test/sort/main.go
+++ test/sort/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	_ "image/jpeg"
 	"image/png"
@@ -13,6 +14,7 @@ import (
 )
 
 type pixels []uint8
+type pixim image.RGBA
 
 func (p pixels) Len() int { return len(p) / 4 * 3 }
 func (p pixels) Less(i, j int) bool {
@@ -29,9 +31,46 @@ func (p pixels) Swap(i, j int) {
 	}
 }
 
+func (p *pixim) Len() int {
+	b := (*image.RGBA)(p).Bounds()
+	return b.Dx() * b.Dy()
+}
+
+func (p *pixim) Less(i, j int) bool {
+	m := (*image.RGBA)(p)
+	b := m.Bounds()
+	iy := i / b.Dx()
+	ix := i % b.Dx()
+	jy := j / b.Dx()
+	jx := j % b.Dx()
+	c1, c2 := m.At(ix, iy).(color.RGBA), m.At(jx, jy).(color.RGBA)
+	dist1 := uint32(c1.R)*uint32(c1.R) + uint32(c1.G)*uint32(c1.G) + uint32(c1.B)*uint32(c1.B)
+	dist2 := uint32(c2.R)*uint32(c2.R) + uint32(c2.G)*uint32(c2.G) + uint32(c2.B)*uint32(c2.B)
+	return dist1 < dist2
+}
+
+func (p *pixim) Swap(i, j int) {
+	m := (*image.RGBA)(p)
+	b := m.Bounds()
+	iy := i / b.Dx()
+	ix := i % b.Dx()
+	jy := j / b.Dx()
+	jx := j % b.Dx()
+	pi := m.PixOffset(ix, iy)
+	pj := m.PixOffset(jx, jy)
+	m.Pix[pi+0], m.Pix[pj+0] = m.Pix[pj+0], m.Pix[pi+0]
+	m.Pix[pi+1], m.Pix[pj+1] = m.Pix[pj+1], m.Pix[pi+1]
+	m.Pix[pi+2], m.Pix[pj+2] = m.Pix[pj+2], m.Pix[pi+2]
+	m.Pix[pi+3], m.Pix[pj+3] = m.Pix[pj+3], m.Pix[pi+3]
+	if counter += 1; counter%1000 == 0 {
+		saveImage(sorting, fmt.Sprintf("%06d.png", counter/1000))
+	}
+}
+
 var (
 	sorting *image.RGBA
 	counter = int(0)
+	method  = flag.Int("m", 0, "the method to use (0=planar, 1=pixel)")
 )
 
 func saveImage(m image.Image, name string) {
@@ -56,9 +95,15 @@ func makeRGB(m image.Image) *image.RGBA {
 	return rgb
 }
 
-func sortimage(m image.Image) image.Image {
+func sortimage0(m image.Image) image.Image {
 	sorting = makeRGB(m)
 	sort.Sort(pixels(sorting.Pix))
+	return sorting
+}
+
+func sortimage1(m image.Image) image.Image {
+	sorting = makeRGB(m)
+	sort.Sort((*pixim)(sorting))
 	return sorting
 }
 
@@ -84,7 +129,15 @@ func main() {
 	}
 
 	log.Print("Sorting image")
-	im := sortimage(inimg)
+	var im image.Image
+	switch *method {
+	case 0:
+		im = sortimage0(inimg)
+	case 1:
+		im = sortimage1(inimg)
+	default:
+		log.Fatalf("invalid method: %v", *method)
+	}
 
 	saveImage(im, flag.Arg(1))
 }
