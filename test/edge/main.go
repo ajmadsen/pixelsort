feceb61ab780db645ea1737f3e209ed0d555fbca
Adam Madsen <madsen.adam@gmail.com>
2014-05-27 18:38:30 -0700

    Sobel is faster

--- test/edge/main.go
+++ test/edge/main.go
@@ -11,170 +11,62 @@ import (
 	"log"
 	"math"
 	"os"
-	"runtime"
 )
 
 var (
-	offsets = []image.Point{
-		image.Point{-1, -1},
-		image.Point{+0, -1},
-		image.Point{+1, -1},
-		image.Point{-1, +0},
-		image.Point{+1, +0},
-		image.Point{-1, +1},
-		image.Point{+0, +1},
-		image.Point{+1, +1},
+	kernX = [][]int{
+		[]int{-1, +0, +1},
+		[]int{-2, +0, +2},
+		[]int{-1, +0, +1},
 	}
+	kernY = [][]int{
+		[]int{+1, +2, +1},
+		[]int{+0, +0, +0},
+		[]int{-1, -2, -1},
+	}
+	kernR = image.Rect(-1, -1, 2, 2)
 )
 
-type hist []int
-
-func newHist(size int) hist {
-	h := make(hist, size)
-	for i := range h {
-		h[i] = 0
-	}
-	return h
-}
-
-type workUnit struct {
-	filename string
-	image    *image.Gray
-	min, max int
-}
-
-func thresholdAndSave(work <-chan workUnit, kill <-chan chan<- int) {
-	for {
-		select {
-		case u := <-work:
-			log.Printf("Thresholding [%v, %v]", u.min, u.max)
-
-			outfile, err := os.Create(u.filename)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			thresh := scaleImage(u.image, u.min, u.max)
-			invert(thresh)
-
-			log.Printf("Saving %s", u.filename)
-			err = png.Encode(outfile, thresh)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			err = outfile.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		case quit := <-kill:
-			quit <- 1
-			return
-		}
-	}
-}
-
-func findThreshold(h hist) (int, int) {
-	min, max := int(-1), int(-1)
-	total := uint64(0)
-	for i := range h {
-		total += uint64(h[i])
-	}
-	cnt := uint64(0)
-	for i := range h {
-		cnt += uint64(h[i])
-		if h[i] > 0 && min < 0 {
-			min = i
-		} else if min >= 0 && h[i] == 0 && max < 0 {
-			max = i-1
-		}
-	}
-	if min > max {
-		return max, min
-	}
-	return min, max
-}
-
-func cDiff(c1, c2 color.Color) uint8 {
-	g1 := c1.(color.Gray)
-	g2 := c2.(color.Gray)
-	if g1.Y > g2.Y {
-		return g1.Y - g2.Y
-	}
-	return g2.Y - g1.Y
-}
-
-func imageGrad(m *image.Gray) (*image.Gray, hist) {
+func convolve(m *image.Gray, kernX, kernY [][]int, r image.Rectangle) uint8 {
+	d := r.Sub(kernR.Min)
 	b := m.Bounds()
-	imrgb := image.NewGray(b)
 
-	h := newHist(math.MaxUint8 + 1)
-	for p := b.Min; p.Y < b.Max.Y; p.Y = p.Y + 1 {
-		for p.X = b.Min.X; p.X < b.Max.X; p.X += 1 {
-			diff := int32(0)
-			c := int32(0)
-			for _, off := range offsets {
-				pt := p.Add(off)
-				if pt.In(b) {
-					c++
-					diff += int32(cDiff(m.At(p.X, p.Y), m.At(pt.X, pt.Y)))
-				}
+	magx := int(0)
+	magy := int(0)
+	for y := d.Min.Y; y < d.Max.Y; y++ {
+		for x := d.Min.X; x < d.Max.X; x++ {
+			if (kernX[y][x] == 0) && (kernY[y][x] == 0) {
+				continue
 			}
-			diff /= c
-
-			h[diff] += 1
-
-			// diff should be in the range of [0.0, 1.0], indicating
-			// the degree of difference between the neighboring pixels
-			// and the current one
-			//log.Print(diff)
-			imrgb.Set(p.X, p.Y, color.Gray{uint8(diff)})
+			v := m.At(b.Min.X+x, b.Min.Y+y).(color.Gray)
+			magx += int(v.Y) * kernX[y][x]
+			magy += int(v.Y) * kernY[y][x]
 		}
 	}
+	mag := math.Sqrt(float64(magx*magx) + float64(magy*magy))
 
-	return imrgb, h
-}
-
-func threshold(x, min, max int) uint8 {
-	x = (x - min) * math.MaxUint8 / max
-	if x < 0 {
-		x = 0
+	if mag < 0 {
+		return 0
 	}
-	return uint8(x)
-}
-
-func bw(x, min int) uint8 {
-	if x < min {
-		return uint8(0)
+	if mag > 255 {
+		return 255
 	}
-	return uint8(255)
+	return uint8(mag)
 }
 
-func scaleImage(m *image.Gray, min, max int) *image.Gray {
+func sobel(m *image.Gray) *image.Gray {
 	b := m.Bounds()
 	im := image.NewGray(b)
-	max -= min
 
-	for j := b.Min.Y; j < b.Max.Y; j++ {
-		for i := b.Min.X; i < b.Max.X; i++ {
-			c := m.At(i, j).(color.Gray)
-			c.Y = bw(int(c.Y), min)
-			im.SetGray(i, j, c)
+	for p := b.Min; p.Y < b.Max.Y; p.Y++ {
+		for p.X = b.Min.X; p.X < b.Max.X; p.X++ {
+			window := kernR.Add(p).Intersect(b)
+			mag := math.MaxUint8 - convolve(m.SubImage(window).(*image.Gray), kernX, kernY, window.Sub(p))
+			im.SetGray(p.X, p.Y, color.Gray{mag})
 		}
 	}
 
 	return im
-}
-
-func invert(m *image.Gray) {
-	b := m.Bounds()
-	for j := b.Min.Y; j < b.Max.Y; j++ {
-		for i := b.Min.X; i < b.Max.X; i++ {
-			c := m.At(i, j).(color.Gray)
-			c.Y = math.MaxUint8 - c.Y
-			m.SetGray(i, j, c)
-		}
-	}
 }
 
 func main() {
@@ -203,31 +95,19 @@ func main() {
 	gray := image.NewGray(b)
 	draw.Draw(gray, b, inimg, b.Min, draw.Src)
 
+	//g, err := os.Create("gray.png")
+	//png.Encode(g, gray)
+
 	log.Println("Computing pixel gradient")
-	grad, h := imageGrad(gray)
-	min, max := findThreshold(h)
+	s := sobel(gray)
 
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	work := make(chan workUnit)
-	kill := make(chan chan<- int, runtime.NumCPU())
-
-	for i := 0; i < runtime.NumCPU(); i++ {
-		go thresholdAndSave(work, kill)
+	log.Println("Writing output")
+	outfile, err := os.Create(flag.Arg(1))
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	for i := 0; i < max-min; i++ {
-		work <- workUnit{
-			filename: fmt.Sprintf(flag.Arg(1), i),
-			image:    grad,
-			min:      min + i,
-			max:      max,
-		}
-	}
-
-	for i := 0; i < cap(kill); i++ {
-		quit := make(chan int)
-		kill <- quit
-		<-quit
+	err = png.Encode(outfile, s)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
